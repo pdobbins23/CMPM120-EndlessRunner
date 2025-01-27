9b995d2e224d270c27afd4b97bbffb085a88fec3
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-01-27 14:24:10 -0800

    Setup main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Name: Peter Dobbins
+// Title: Sonic Boom
+// Date: 27/1/25
+// Time Spent: ???
+
+"use strict";
+
+let config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 480,
+  scene: [],
+};
+
+let game = new Phaser.Game(config);
