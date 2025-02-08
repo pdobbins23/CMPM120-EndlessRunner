fcb557846edb958e942f1854dff41c5a7d1a7632
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-02-07 17:04:17 -0800

    Working on widthmaps

--- assets/Main.tsx
+++ assets/Main.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Main" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+<tileset version="1.10" tiledversion="1.11.2" name="Main" tilewidth="32" tileheight="32" tilecount="96" columns="12">
  <image source="img/grid.png" width="384" height="256"/>
  <tile id="4">
   <properties>
@@ -27,33 +27,29 @@
  <tile id="8">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="heightmap" type="int" value="2"/>
+   <property name="hwmap" type="int" value="2"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="2"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="heightmap" type="int" value="3"/>
+   <property name="hwmap" type="int" value="3"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="3"/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="heightmap" type="int" value="4"/>
+   <property name="hwmap" type="int" value="4"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="4"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
    <property name="ground_angle" type="int" value="0"/>
-   <property name="heightmap" type="int" value="0"/>
+   <property name="hwmap" type="int" value="0"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="0"/>
   </properties>
  </tile>
  <tile id="16">
@@ -69,17 +65,15 @@
  <tile id="22">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="heightmap" type="int" value="5"/>
+   <property name="hwmap" type="int" value="5"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="5"/>
   </properties>
  </tile>
  <tile id="23">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="heightmap" type="int" value="6"/>
+   <property name="hwmap" type="int" value="6"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="6"/>
   </properties>
  </tile>
  <tile id="28">
@@ -91,26 +85,23 @@
   <properties>
    <property name="flipmap" type="bool" value="true"/>
    <property name="ground_angle" type="float" value="5.02654825"/>
-   <property name="heightmap" type="int" value="7"/>
+   <property name="hwmap" type="int" value="7"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="7"/>
   </properties>
  </tile>
  <tile id="36">
   <properties>
    <property name="ground_angle" type="float" value="0.78539816"/>
-   <property name="heightmap" type="int" value="1"/>
+   <property name="hwmap" type="int" value="1"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="1"/>
   </properties>
  </tile>
  <tile id="37">
   <properties>
    <property name="flipmap" type="bool" value="true"/>
-   <property name="ground_angle" type="float" value="2.35619449"/>
-   <property name="heightmap" type="int" value="1"/>
+   <property name="ground_angle" type="float" value="5.49778714"/>
+   <property name="hwmap" type="int" value="1"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="1"/>
   </properties>
  </tile>
  <tile id="40">
@@ -122,9 +113,8 @@
   <properties>
    <property name="flipmap" type="bool" value="true"/>
    <property name="ground_angle" type="float" value="4.85518865"/>
-   <property name="heightmap" type="int" value="8"/>
+   <property name="hwmap" type="int" value="8"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="8"/>
   </properties>
  </tile>
  <tile id="52">
@@ -135,9 +125,8 @@
  <tile id="59">
   <properties>
    <property name="ground_angle" type="float" value="1.42799666"/>
-   <property name="heightmap" type="int" value="8"/>
+   <property name="hwmap" type="int" value="8"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="8"/>
   </properties>
  </tile>
  <tile id="64">
@@ -148,9 +137,8 @@
  <tile id="71">
   <properties>
    <property name="ground_angle" type="float" value="1.25663706"/>
-   <property name="heightmap" type="int" value="7"/>
+   <property name="hwmap" type="int" value="7"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="7"/>
   </properties>
  </tile>
  <tile id="76">
@@ -166,17 +154,15 @@
  <tile id="82">
   <properties>
    <property name="ground_angle" type="float" value="0.78539816"/>
-   <property name="heightmap" type="int" value="5"/>
+   <property name="hwmap" type="int" value="5"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="5"/>
   </properties>
  </tile>
  <tile id="83">
   <properties>
    <property name="ground_angle" type="float" value="1.04719755"/>
-   <property name="heightmap" type="int" value="6"/>
+   <property name="hwmap" type="int" value="6"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="6"/>
   </properties>
  </tile>
  <tile id="88">
@@ -203,25 +189,22 @@
  <tile id="92">
   <properties>
    <property name="ground_angle" type="float" value="0.15707963"/>
-   <property name="heightmap" type="int" value="2"/>
+   <property name="hwmap" type="int" value="2"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="2"/>
   </properties>
  </tile>
  <tile id="93">
   <properties>
    <property name="ground_angle" type="float" value="0.62831853"/>
-   <property name="heightmap" type="int" value="3"/>
+   <property name="hwmap" type="int" value="3"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="3"/>
   </properties>
  </tile>
  <tile id="94">
   <properties>
    <property name="ground_angle" type="float" value="0.78539816"/>
-   <property name="heightmap" type="int" value="4"/>
+   <property name="hwmap" type="int" value="4"/>
    <property name="solid" type="bool" value="true"/>
-   <property name="widthmap" type="int" value="4"/>
   </properties>
  </tile>
 </tileset>
--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -16,14 +16,6 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     
     this.rolling = false;
 
-    // sensors
-    this.sensorBL = {offsetX: -0.75, offsetY: 1, dirX: 0, dirY: 1};
-    this.sensorBR = {offsetX: 0.75, offsetY: 1, dirX: 0, dirY: 1};
-    this.sensorML = {offsetX: -0.75, offsetY: 0, dirX: -1, dirY: 0};
-    this.sensorMR = {offsetX: 0.75, offsetY: 0, dirX: 1, dirY: 0};
-    this.sensorTL = {offsetX: -0.75, offsetY: -1, dirX: 0, dirY: -1};
-    this.sensorTR = {offsetX: 0.75, offsetY: -1, dirX: 0, dirY: -1};
-
     // Setup animations
     this.anims.create({
       key: "run",
@@ -59,24 +51,70 @@ class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   update() {
-    let sensorBLPos = {x: this.x + this.sensorBL.offsetX * (this.width / 2), y: this.y + this.sensorBL.offsetY * (this.height / 2)};
-    let sensorBRPos = {x: this.x + this.sensorBR.offsetX * (this.width / 2), y: this.y + this.sensorBR.offsetY * (this.height / 2)};
-    let sensorMLPos = {x: this.x + this.sensorML.offsetX * (this.width / 2), y: this.y + this.sensorML.offsetY * (this.height / 2)};
+    const heightmaps = [
+      [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5],
+      [5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18],
+      [19, 19, 20, 21, 21, 22, 23, 23, 24, 25, 26, 26, 27, 28, 29, 29, 30, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13],
+      [14, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 29, 30, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 10, 12, 14, 16, 19, 22, 24, 28, 31, 32, 32, 32, 32, 32],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 13, 21, 32],
+    ];
+    const widthmaps = [
+      [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
+      [32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      [32, 21, 13, 7, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [32, 32, 32, 32, 32, 31, 28, 24, 22, 19, 16, 14, 12, 10, 8, 6, 4, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 30, 29, 27, 26, 24, 23, 22, 20, 19, 18, 16, 15, 14],
+      [13, 12, 11, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [],
+      [],
+    ];
+
+    const sensorBLoffset = {x: -0.75, y: 1};
+    const sensorBRoffset = {x: 0.75, y: 1};
+    
+    let sensorBLPos = undefined;
+    let sensorBRPos = undefined;
+    let sensorBDir = undefined;
+    let hwmap = undefined;
+
+    if ((0 <= this.groundAngle && this.groundAngle <= 0.78539816) || (5.49778714 <= this.groundAngle && this.groundAngle <= 6.28318531)) { // floor mode
+      sensorBLPos = {x: this.x + sensorBLoffset.x * (this.width / 2), y: this.y + sensorBLoffset.y * (this.height / 2)};
+      sensorBRPos = {x: this.x + sensorBRoffset.x * (this.width / 2), y: this.y + sensorBRoffset.y * (this.height / 2)};
+      sensorBDir = {x: 0, y: 1};
+
+      hwmap = heightmaps;
+
+      this.setRotation(0);
+    } else if ((0.78539816 < this.groundAngle && this.groundAngle <  2.35619449)) { // right wall
+      sensorBLPos = {x: this.x + sensorBLoffset.y * (this.height / 2), y: this.y - sensorBLoffset.x * (this.width / 2)};
+      sensorBRPos = {x: this.x + sensorBRoffset.y * (this.height / 2), y: this.y + sensorBRoffset.x * (this.width / 2)};
+      sensorBDir = {x: 1, y: 0};
+
+      hwmap = widthmaps;
+
+      this.setRotation(-Math.PI / 2);
+    }
+    
+    /*let sensorMLPos = {x: this.x + this.sensorML.offsetX * (this.width / 2), y: this.y + this.sensorML.offsetY * (this.height / 2)};
     let sensorMRPos = {x: this.x + this.sensorMR.offsetX * (this.width / 2), y: this.y + this.sensorMR.offsetY * (this.height / 2)};
     let sensorTLPos = {x: this.x + this.sensorTL.offsetX * (this.width / 2), y: this.y + this.sensorTL.offsetY * (this.height / 2)};
-    let sensorTRPos = {x: this.x + this.sensorTR.offsetX * (this.width / 2), y: this.y + this.sensorTR.offsetY * (this.height / 2)};
+    let sensorTRPos = {x: this.x + this.sensorTR.offsetX * (this.width / 2), y: this.y + this.sensorTR.offsetY * (this.height / 2)};*/
 
     // TODO: Handle rotations?
 
     // debug stuff
     this.scene.graphics.fillStyle(0xFF0000, 1);
     this.scene.graphics.fillRect(this.x, this.y, 2, 2);
-    this.scene.graphics.fillRect(sensorBLPos.x, sensorBLPos.y, 2, 2);
-    this.scene.graphics.fillRect(sensorBRPos.x, sensorBRPos.y, 2, 2);
-    this.scene.graphics.fillRect(sensorMLPos.x, sensorMLPos.y, 2, 2);
-    this.scene.graphics.fillRect(sensorMRPos.x, sensorMRPos.y, 2, 2);
-    this.scene.graphics.fillRect(sensorTLPos.x, sensorTLPos.y, 2, 2);
-    this.scene.graphics.fillRect(sensorTRPos.x, sensorTRPos.y, 2, 2);
+    this.scene.graphics.fillRect(sensorBLPos.x, sensorBLPos.y, 3, 3);
+    this.scene.graphics.fillRect(sensorBRPos.x, sensorBRPos.y, 3, 3);
+    // this.scene.graphics.fillRect(sensorMLPos.x, sensorMLPos.y, 2, 2);
+    // this.scene.graphics.fillRect(sensorMRPos.x, sensorMRPos.y, 2, 2);
+    // this.scene.graphics.fillRect(sensorTLPos.x, sensorTLPos.y, 2, 2);
+    // this.scene.graphics.fillRect(sensorTRPos.x, sensorTRPos.y, 2, 2);
 
     // tileset
     const ts = this.scene.chunks[0].map.getTileset("Main");
@@ -93,23 +131,11 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     let tileBL = this.scene.chunks[0].map.getTileAt(tileBLIndex.x, tileBLIndex.y, true, "Layer0");
     let tileBR = this.scene.chunks[0].map.getTileAt(tileBRIndex.x, tileBRIndex.y, true, "Layer0");
 
-    const heightmaps = [
-      [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5],
-      [5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18],
-      [19, 19, 20, 21, 21, 22, 23, 23, 24, 25, 26, 26, 27, 28, 29, 29, 30, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13],
-      [14, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 29, 30, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32]
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 10, 12, 14, 16, 19, 22, 24, 28, 31, 32, 32, 32, 32, 32],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 13, 21, 32]
-    ];
-
     let tileBLdy = undefined;
     let tileBRdy = undefined;
 
     if (tileBL.properties.solid) {
-      let hm = heightmaps[tileBL.properties.heightmap];
+      let hm = hwmap[tileBL.properties.heightmap];
 
       let tileX = this.scene.chunks[0].layer.x + tileBL.x * 32;
       let tileY = this.scene.chunks[0].layer.y + tileBL.y * 32 + 32;
@@ -119,12 +145,12 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
       if (idx == 0) {
         // extension
-        let etileBL = this.scene.chunks[0].map.getTileAt(tileBLIndex.x, tileBLIndex.y + 1, true, "Layer0");
+        let etileBL = this.scene.chunks[0].map.getTileAt(tileBLIndex.x + sensorBDir.x, tileBLIndex.y + sensorBDir.y, true, "Layer0");
 
         if (etileBL !== null && etileBL.properties.solid) {
           tileBL = etileBL;
           
-          hm = heightmaps[tileBL.properties.heightmap];
+          hm = hwmap[tileBL.properties.heightmap];
 
           tileX = this.scene.chunks[0].layer.x + tileBL.x * 32;
           tileY = this.scene.chunks[0].layer.y + tileBL.y * 32 + 32;
@@ -134,12 +160,12 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         }
       } else if (idx == 32) {
         // regression
-        let rtileBL = this.scene.chunks[0].map.getTileAt(tileBLIndex.x, tileBLIndex.y - 1, true, "Layer0");
+        let rtileBL = this.scene.chunks[0].map.getTileAt(tileBLIndex.x - sensorBDir.x, tileBLIndex.y - sensorBDir.y, true, "Layer0");
 
         if (rtileBL !== null && rtileBL.properties.solid) {
           tileBL = rtileBL;
           
-          hm = heightmaps[tileBL.properties.heightmap];
+          hm = hwmap[tileBL.properties.heightmap];
 
           tileX = this.scene.chunks[0].layer.x + tileBL.x * 32;
           tileY = this.scene.chunks[0].layer.y + tileBL.y * 32 + 32;
@@ -151,7 +177,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
       
       let dy = tileY - sensorBLPos.y - idx;
 
-      console.log(dx, hm[dx], dy);
+      console.log(dx, idx, dy);
 
       if (dy < 0) {
         tileBLdy = dy;
@@ -159,7 +185,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     }
     
     if (tileBR.properties.solid) {
-      let hm = heightmaps[tileBR.properties.heightmap];
+      let hm = hwmap[tileBR.properties.heightmap];
 
       let tileX = this.scene.chunks[0].layer.x + tileBR.x * 32;
       let tileY = this.scene.chunks[0].layer.y + tileBR.y * 32 + 32;
@@ -169,12 +195,12 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
       if (idx == 0) {
         // extension
-        let etileBR = this.scene.chunks[0].map.getTileAt(tileBRIndex.x, tileBRIndex.y + 1, true, "Layer0");
+        let etileBR = this.scene.chunks[0].map.getTileAt(tileBRIndex.x + sensorBDir.x, tileBRIndex.y + sensorBDir.y, true, "Layer0");
 
         if (etileBR !== null && etileBR.properties.solid) {
           tileBR = etileBR;
           
-          hm = heightmaps[tileBR.properties.heightmap];
+          hm = hwmap[tileBR.properties.heightmap];
 
           tileX = this.scene.chunks[0].layer.x + tileBR.x * 32;
           tileY = this.scene.chunks[0].layer.y + tileBR.y * 32 + 32;
@@ -184,12 +210,12 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         }
       } else if (idx == 32) {
         // regression
-        let rtileBR = this.scene.chunks[0].map.getTileAt(tileBRIndex.x, tileBRIndex.y - 1, true, "Layer0");
+        let rtileBR = this.scene.chunks[0].map.getTileAt(tileBRIndex.x - sensorBDir.x, tileBRIndex.y - sensorBDir.y, true, "Layer0");
 
         if (rtileBR !== null && rtileBR.properties.solid) {
           tileBR = rtileBR;
           
-          hm = heightmaps[tileBR.properties.heightmap];
+          hm = hwmap[tileBR.properties.heightmap];
 
           tileX = this.scene.chunks[0].layer.x + tileBR.x * 32;
           tileY = this.scene.chunks[0].layer.y + tileBR.y * 32 + 32;
